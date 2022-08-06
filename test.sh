add07721b50f682368f54ea9859f20b26eef31a4
Hans de Goede <hdegoede@redhat.com>
2022-08-06 15:15:17 +0200

    Add various byt/cht atomisp2 test scripts

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+FRAMES=10
+
+FORMAT=NV12
+WIDTH=1600
+HEIGHT=1200
+
+#FORMAT=NV12
+#WIDTH=1920
+#HEIGHT=1080
+
+
+./v4l2n -o testimage_@.raw \
+                 --device /dev/video0 \
+                 --input 0 \
+                 --exposure=2000,0,300,0 \
+                 --parm type=1,capturemode=CI_MODE_PREVIEW \
+                 --fmt type=1,width=$WIDTH,height=$HEIGHT,pixelformat=$FORMAT \
+                 --reqbufs count=2,memory=USERPTR \
+                 --parameters=wb_config.r=32768,wb_config.gr=21043,wb_config.gb=21043,wb_config.b=30863 \
+                 --capture=$FRAMES
+
+for i in $(seq 0 $(($FRAMES - 1))); do
+        name="testimage_$(printf "%03i" $i)"
+
+        ./raw2pnm -x$WIDTH -y$HEIGHT -f$FORMAT $name.raw $name.pnm
+
+        rm $name.raw
+done                 
