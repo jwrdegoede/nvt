cbe90102684ceef499836223677e3af2a97f0f56
Hans de Goede <hdegoede@redhat.com>
2022-08-07 23:12:31 +0200

    Update byt/cht atomisp2 test scripts

--- test-ov2680-front.sh
+++ test-ov2680-front.sh
@@ -4,22 +4,21 @@ set -e
 FRAMES=10
 
 FORMAT=NV12
-#WIDTH=1600
-#HEIGHT=1200
-WIDTH=640
-HEIGHT=480
+WIDTH=1600
+HEIGHT=1200
 
 #FORMAT=NV12
 #WIDTH=1920
 #HEIGHT=1080
 
+modprobe atomisp
 
 ./v4l2n -o testimage_@.raw \
-                 --device /dev/video2 \
+                 --device /dev/video0 \
                  --input 1 \
                  --parm type=1,capturemode=CI_MODE_PREVIEW \
                  --fmt type=1,width=$WIDTH,height=$HEIGHT,pixelformat=$FORMAT \
-                 --reqbufs count=2,memory=MMAP \
+                 --reqbufs count=2,memory=USERPTR \
                  --parameters=wb_config.r=32768,wb_config.gr=21043,wb_config.gb=21043,wb_config.b=30863 \
                  --capture=$FRAMES
 
